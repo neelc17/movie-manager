3bed67ee49218af5903295d111b447aa9475db12
Neel Chaudhari <neelch1154@gmail.com>
2022-02-08 01:16:15 -0800

    [App] basic uid check

--- server/frontend/src/App/App.js
+++ server/frontend/src/App/App.js
@@ -1,20 +1,56 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import MMNavbar from "../components/MMNavbar/MMNavbar";
 import Home from "../components/Home/Home";
+import EnterMovie from "../components/EnterMovie/EnterMovie";
+import MovieList from "../components/MovieList/MovieList";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MMNavbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  const [loading, setLoading] = useState(true);
+  const [unauth, setUnauth] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get("uid") !== null) {
+      localStorage.setItem("uid", params.get("uid"));
+    }
+    if (!localStorage.getItem("uid")) {
+      setUnauth(true);
+    }
+    else {
+      setUnauth(false);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+  else {
+    if (unauth) {
+      return (
+        <div>Unauthorized</div>
+      );
+    }
+    else {
+      return (
+        <BrowserRouter>
+          <div className="App">
+            <MMNavbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/entermovie" element={<EnterMovie />} />
+              <Route path="/movielist" element={<MovieList />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      );
+    }
+  }
 }
 
 export default App;
