21e050ba2f77ee6cebd8f37e253f8a9f4cc08426
Neel Chaudhari <neelch1154@gmail.com>
2022-02-10 01:39:38 -0800

    [backend] added user check login route

--- server/backend/routes/login.js
+++ server/backend/routes/login.js
@@ -5,6 +5,23 @@ const userexists = (uid, callback) => {
   db.query(`select uid from Users where uid = "${uid}";`, callback);
 };
 
+router.post("/checkuser", (req, res) => {
+  if (!req.body.uid) {
+    res.status(400).send("No UID provided");
+  }
+  else {
+    userexists(req.body.uid, (err, data) => {
+      if (err) {
+        console.log("Error checking user", err);
+        res.status(500).send(err);
+      }
+      else {
+        res.send(data.length > 0);
+      }
+    });
+  }
+});
+
 module.exports = {
   router: router,
   userexists: userexists,
