796d16e687ba261eca7b6c92fa9331c7791e9824
Neel Chaudhari <neelch1154@gmail.com>
2022-02-10 01:52:51 -0800

    [EnterMovie] added basic form validation and functionality

--- server/frontend/src/components/EnterMovie/EnterMovie.js
+++ server/frontend/src/components/EnterMovie/EnterMovie.js
@@ -2,7 +2,7 @@ import { Form, Button, Dropdown } from "react-bootstrap";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function EnterMovie() {
+function EnterMovie(props) {
   const [loading, setLoading] = useState(true);
   const [prefillData, setPrefillData] = useState({
     genres: [],
@@ -11,11 +11,7 @@ function EnterMovie() {
     prodComps: [],
     directors: [],
   });
-  // const [genres, setGenres] = useState([]);
-  // const [countries, setCountries] = useState([]);
-  // const [distComps, setDistComps] = useState([]);
-  // const [prodComps, setProdComps] = useState([]);
-  // const [directors, setDirectors] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
 
   useEffect(() => {
     axios.get("/api/getformprefill")
@@ -27,8 +23,73 @@ function EnterMovie() {
     });
   }, []);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    const results = e.target;
+    e.preventDefault();
 
+    let errors = "";
+    for (let i of results.formDates.value.trim().split(",")) {
+      if (isNaN(i) || i.length !== 8) {
+        errors += "Invalid date(s) watched\n"
+        break;
+      }
+    }
+    if (isNaN(results.formRuntime.value.trim())) {
+      errors += "Invalid runtime\n"
+    }
+    if (isNaN(results.formYear.value.trim()) || results.formYear.value.trim().length !== 4) {
+      errors += "Invalid year\n"
+    }
+    if (errors !== "") {
+      console.log(errors);
+      setErrorMsg(errors);
+      return;
+    }
+    
+    const dates = results.formDates.value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const title = results.formTitle.value.trim();
+    const genres = document.getElementById("genretext").value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const runtime = parseInt(results.formRuntime.value.trim());
+    const year = results.formYear.value.trim();
+    const rating = results.formRating.value;
+    const countries = document.getElementById("countrytext").value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const distComps = document.getElementById("disttext").value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const prodComps = document.getElementById("prodtext").value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const directors = document.getElementById("directtext").value.trim().split(",").filter((i) => {
+      return i !== "";
+    });
+    const erate = parseInt(results.formErate.value);
+    const orate = parseInt(results.formOrate.value);
+    axios.post("/api/entermovie", {
+      uid: props.uid,
+      dates: dates,
+      title: title,
+      genres: genres,
+      runtime: runtime,
+      year: year,
+      rating: rating,
+      countries: countries,
+      distComps: distComps,
+      prodComps: prodComps,
+      directors: directors,
+      erate: erate,
+      orate: orate,
+    }).then((res) => {
+      setErrorMsg("Submitted successfully");
+    }).catch((err) => {
+      console.log(err.response);
+      setErrorMsg(`Error submitting: ${err.response.status}, ${err.response.data}`);
+    });
   };
 
   if (loading) {
@@ -40,15 +101,20 @@ function EnterMovie() {
     return (
       <div>
         <h1>Enter Movie</h1>
+        {errorMsg !== "" && (
+          <div>
+            {errorMsg.split("\n").map((item) => (<p>{item}</p>))}
+          </div>
+        )}
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formDates">
             <Form.Label>Date(s) Watched</Form.Label>
-            <Form.Control placeholder="MMDDYYYY,MMDDYYYY" />
+            <Form.Control required />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formTitle">
             <Form.Label>Title</Form.Label>
-            <Form.Control />
+            <Form.Control required />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formGenres">
@@ -61,17 +127,28 @@ function EnterMovie() {
                 ))}
               </Dropdown.Menu>
             </Dropdown>
-            <Form.Control id="genretext" />
+            <Form.Control required id="genretext" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formRuntime">
             <Form.Label>Runtime</Form.Label>
-            <Form.Control />
+            <Form.Control required />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formYear">
             <Form.Label>Year</Form.Label>
-            <Form.Control />
+            <Form.Control required />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formRating">
+            <Form.Label>MPAA Rating</Form.Label>
+            <Form.Select required>
+              <option value="G">G</option>
+              <option value="PG">PG</option>
+              <option value="PG-13">PG-13</option>
+              <option value="R">R</option>
+              <option value="NC-17">NC-17</option>
+            </Form.Select>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formCountries">
@@ -84,7 +161,7 @@ function EnterMovie() {
                 ))}
               </Dropdown.Menu>
             </Dropdown>
-            <Form.Control id="countrytext" />
+            <Form.Control required id="countrytext" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formDistComps">
@@ -97,7 +174,7 @@ function EnterMovie() {
                 ))}
               </Dropdown.Menu>
             </Dropdown>
-            <Form.Control id="disttext" />
+            <Form.Control required id="disttext" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formProdComps">
@@ -110,7 +187,7 @@ function EnterMovie() {
                 ))}
               </Dropdown.Menu>
             </Dropdown>
-            <Form.Control id="prodtext" />
+            <Form.Control required id="prodtext" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formDirectors">
@@ -123,7 +200,41 @@ function EnterMovie() {
                 ))}
               </Dropdown.Menu>
             </Dropdown>
-            <Form.Control id="directtext" />
+            <Form.Control required id="directtext" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formErate">
+            <Form.Label>Enjoyment Rating</Form.Label>
+            <Form.Select required>
+              <option value="10">A+</option>
+              <option value="9">A</option>
+              <option value="8">A-</option>
+              <option value="7">B+</option>
+              <option value="6">B</option>
+              <option value="5">B-</option>
+              <option value="4">C+</option>
+              <option value="3">C</option>
+              <option value="2">C-</option>
+              <option value="1">D</option>
+              <option value="0">F</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formOrate">
+            <Form.Label>Overall Rating</Form.Label>
+            <Form.Select required>
+              <option value="10">A+</option>
+              <option value="9">A</option>
+              <option value="8">A-</option>
+              <option value="7">B+</option>
+              <option value="6">B</option>
+              <option value="5">B-</option>
+              <option value="4">C+</option>
+              <option value="3">C</option>
+              <option value="2">C-</option>
+              <option value="1">D</option>
+              <option value="0">F</option>
+            </Form.Select>
           </Form.Group>
 
           <Button variant="primary" type="submit">Submit</Button>
